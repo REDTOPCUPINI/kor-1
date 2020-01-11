ef203c2f8cf39bc622c65ee63d462e4d37b12f60
grorkwkwq <58606213+grorkwkwq@users.noreply.github.com>
2020-01-12 00:11:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $GLOBALS['_ta_debug_mode'] = false; //To enable debug mode, set to true or load 
 
 require 'bootloader.php';
 
-$campaign_id = 'fbe893';
+$campaign_id = '5c7ie4';
 
 $ta = new TALoader($campaign_id);
 
